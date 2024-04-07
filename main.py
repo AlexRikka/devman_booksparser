58990a21288ea2371eeae2c2d6d46da76fc8244d
AlexRikka <rikka.gold@mail.ru>
2024-04-08 00:29:16 +0400

    add HTTPError

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ import requests
 import os
 
 
+def download_book(id):
+    url = f'https://tululu.org/txt.php?id={id}'
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    with open(f'books/id{id}.txt', 'wb') as f:
+        f.write(response.content)
+
+
+def check_for_redirect(response):
+    if len(response.history) != 0:
+        raise requests.HTTPError
+
+
 def main():
     os.makedirs('books', exist_ok=True)
     for i in range(1, 11):
-        url = 'https://tululu.org/txt.php?id=1'
-        response = requests.get(url)
-        response.raise_for_status()
-        with open(f'books/id{i}.txt', 'wb') as f:
-            f.write(response.content)
+        try:
+            download_book(i)
+        except requests.HTTPError:
+            pass
 
 
 if __name__ == '__main__':
