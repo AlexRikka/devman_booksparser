be0bd02f971a2550fe4db14beb43fe45883d6c2a
AlexRikka <rikka.gold@mail.ru>
2024-05-06 01:52:23 +0400

    download book covers

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import requests
 import os
 from bs4 import BeautifulSoup
-
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit
 
 
 def check_for_redirect(response):
     if len(response.history) != 0:
         raise requests.HTTPError
+
+
+def download_image(url,  folder='images/'):
+    """Скачивает картинки.
+
+    Args:
+      url (str): Cсылка на картинку для скачивания.
+      folder (str): Папка, куда сохранять.
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    img_path = folder + urlsplit(url).path.split('/')[-1]
+    with open(img_path, 'wb') as f:
+        f.write(response.content)
 
 
 def download_txt(url, filename, folder='books/'):
@@ -41,7 +56,7 @@ def download_book(id):
     url = f'https://tululu.org/b{id}/'
     response = requests.get(url)
     response.raise_for_status()
-
+    check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
     title_tag = soup.find('table').find('h1')
     title_text = title_tag.text.split('::')[0].strip()
@@ -49,10 +64,14 @@ def download_book(id):
     filename = f'{id}. ' + filename
     txt_url = f'https://tululu.org/txt.php?id={id}'
     download_txt(txt_url, filename, folder='books/')
+    img_src = soup.find('div', class_='bookimage').find('img')['src']
+    img_url = urljoin('https://tululu.org', img_src)
+    download_image(img_url)
 
 
 def main():
     os.makedirs('books', exist_ok=True)
+    os.makedirs('images', exist_ok=True)
     for i in range(1, 11):
         try:
             download_book(i)
